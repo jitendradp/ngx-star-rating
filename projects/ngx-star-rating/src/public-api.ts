ca85b17a2c6abec05369212e481fcf5756f1e56d
Nicolas Viana da Silva <nicolas.viana.silva@outlook.com.br>
2024-01-22 14:30:01 -0300

    h

--- projects/ngx-star-rating/src/public-api.ts
+++ projects/ngx-star-rating/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-star-rating
  */
 
-export * from './lib/ngx-star-rating.service';
-export * from './lib/ngx-star-rating.component';
-export * from './lib/ngx-star-rating.module';
+export { NgxStarRatingModule } from './lib/ngx-star-rating.module';
+export { NgxStarRatingComponent } from './lib/ngx-star-rating.component';
+// export * from "./lib/types"
