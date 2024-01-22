1659b08b298a19dda32396223f1e01c34503ea99
Nicolas Viana da Silva <nicolas.viana.silva@outlook.com.br>
2024-01-22 14:48:08 -0300

    aj

--- projects/ngx-star-rating/src/lib/ngx-star-rating.module.ts
+++ projects/ngx-star-rating/src/lib/ngx-star-rating.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { NgxStarRatingComponent } from './ngx-star-rating.component';
 
 @NgModule({
